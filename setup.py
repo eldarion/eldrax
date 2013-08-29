377568a99ef6f4fc2bc1e398c3ff969bbc927491
Brian Rosner <brosner@gmail.com>
2013-08-29 10:20:52 -0600

    updated requests

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="eldrax",
-    version="0.1a1",
+    version="0.1a2",
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        "requests==1.1.0",
+        "requests==1.2.3",
     ],
 )
